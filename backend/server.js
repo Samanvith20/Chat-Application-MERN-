dec087a187ac5e3b007759277f3eaf046b9bf136
Samanvith <Samanvith2005@gmail.com>
2023-10-18 19:02:39 +0530

    Coonected mongodb to Backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
-const express =require("express")
-const dotenv = require("dotenv")
-const app =express();
-app.get("/",(req,res,) =>{
-   res.send("server Started  successful")
-})
-const PORT = process.env.PORT|| 5000
-app.listen(PORT,console.log(`Server running Succesful on ${PORT}`))
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const app = express();
+
+// Load environment variables from .env file
+dotenv.config();
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Server Started successfully");
+});
+app.use("/api/user", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running successful on ${PORT}`.green.bold));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home";
 import Chatpage from "./pages/Chatpage";
-
+import "./App.css"
 const App = () => {
   return (
+      <div className="App">
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chat" element={<Chatpage />} />
       </Routes>
     </BrowserRouter>
+    </div>
   );
 };
 

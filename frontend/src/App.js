b1778213e38a1a344f7f4dabd686072167aca50f
Samanvith <Samanvith2005@gmail.com>
2023-10-18 14:39:03 +0530

    Installed mongoose

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home";
+import Chatpage from "./pages/Chatpage";
 
 const App = () => {
   return (
-    <div>
-      Hello world
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chatpage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
